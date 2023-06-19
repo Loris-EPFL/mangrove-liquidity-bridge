f0fec446c39608d0ef27dc8907be5890d770e1ff
RedRise <polluxxx@gmail.com>
2023-06-19 23:43:06 +0200

    univ3 init

--- test/DexUniV3.t.sol
+++ test/DexUniV3.t.sol
@@ -8,12 +8,18 @@ import {IERC20} from "mgv_src/MgvLib.sol";
 import {DexUniV3} from "src/DexLogic/DexUniV3.sol";
 import {UD60x18, ud} from "@prb/math/UD60x18.sol";
 import {ERC20Normalizer} from "src/ERC20Normalizer.sol";
+import {IUniswapV3Factory} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import {INonfungiblePositionManager} from "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 
 contract DexUniV3Test is TestContext {
     DexUniV3 dex;
     IERC20 base;
     IERC20 quote;
+    IUniswapV3Factory factory;
     address alice = address(1111);
+    address larry = address(2222); // larry is the liquidity provider
 
     function setUp() public {}
 
@@ -23,10 +29,62 @@ contract DexUniV3Test is TestContext {
         address swapRouteur = loadAddress("UNIV3_ROUTER");
         vm.label(swapRouteur, "UniV3-routeur");
 
-        address factory = loadAddress("UNIV3_FACTORY");
-        vm.label(factory, "UniV3-factory");
+        factory = IUniswapV3Factory(loadAddress("UNIV3_FACTORY"));
 
-        dex = new DexUniV3(swapRouteur, factory, fee);
+        vm.label(address(factory), "UniV3-factory");
+        dex = new DexUniV3(swapRouteur, address(factory), fee);
+    }
+
+    function toQ96(UD60x18 q) internal pure returns (uint160) {
+        uint intPart = (q.div(ud(1e18)) << 96);
+        uint fracPart = (q.mod(ud(1e18)) << 96) / 1e18;
+        return uint160(intPart + fracPart);
+    }
+
+    function checkOrCreatePool(
+        uint24 fee,
+        UD60x18 currentPrice,
+        UD60x18 quoteAmount
+    ) public {
+        // check if pool exists
+        address poolAddr = factory.getPool(address(base), address(quote), fee);
+
+        if (poolAddr != address(0)) {
+            return;
+        }
+
+        console2.log("DexUniV3Test/setUp/creating pool");
+        poolAddr = factory.createPool(address(base), address(quote), fee);
+
+        // initialize pool and deposit
+        IUniswapV3Pool pool = IUniswapV3Pool(poolAddr);
+
+        if (address(base) != pool.token0()) {
+            currentPrice = ud(1e18) / currentPrice;
+        }
+
+        uint160 sqrtPriceX96 = uint160(100000);
+        // TODO convert currentPrice to sqrtPriceX96
+        pool.initialize(sqrtPriceX96);
+
+        INonfungiblePositionManager.MintParams
+            memory params = INonfungiblePositionManager.MintParams({
+                token0: address(base),
+                token1: address(quote),
+                fee: fee,
+                tickLower: TickMath.MIN_TICK,
+                tickUpper: TickMath.MAX_TICK,
+                amount0Desired: N.denormalize(
+                    base,
+                    (quoteAmount.div(currentPrice)).unwrap()
+                ),
+                amount1Desired: N.denormalize(quote, quoteAmount.unwrap()),
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: larry,
+                deadline: block.timestamp
+            });
+        nonfungiblePositionManager.mint(params);
     }
 
     function testWBTC_USD_midPrice_above_10_000() public {
